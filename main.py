fc4d60bb63044233c09886f373afc7a68be089c5
Deltah3 <c.gentili@it-students.fr>
2025-01-29 14:36:14 +0100

    Ajout des templates

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-from flask import Flask
+from app import create_app  
 import numpy as np
 import cv2
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, World! Ceci est ma page principale."
+app = create_app()  
 
 print("NumPy version:", np.__version__)
 print("OpenCV version:", cv2.__version__)
 
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
+
+
+# --------------------------------------------------------
+
+# from flask import Flask
+# import numpy as np
+# import cv2
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#    return "Hello, World! Ceci est ma page principale."
+
+# print("NumPy version:", np.__version__)
+# print("OpenCV version:", cv2.__version__)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=8000)
+
+
+
